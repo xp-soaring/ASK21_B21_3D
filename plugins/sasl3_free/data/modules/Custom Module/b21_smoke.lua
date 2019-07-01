36a0274b5da544b6a280994f2d5f1094e2386c3d
B21 <thriplow@gmail.com>
2019-07-01 09:58:14 +0100

    gmeter startup bugfix for xplane G reading initialization

--- plugins/sasl3_free/data/modules/Custom Module/b21_smoke.lua
+++ plugins/sasl3_free/data/modules/Custom Module/b21_smoke.lua
@@ -1,12 +1,15 @@
 
+-- WRITE datarefs
 local DATAREF_SMOKE = globalPropertyi("sim/cockpit/weapons/missiles_armed") -- X-Plane Particle system not supporting plugin datarefs
 
 print("b21_smoke loading "..get(DATAREF_SMOKE))
 
+-- COMMANDS
 local command_smoke_toggle = sasl.createCommand("b21/smoke/toggle", "Toggle wingtip smoke emitters")
 local command_smoke_on = sasl.createCommand("b21/smoke/on", "Enable wingtip smoke emitters")
 local command_smoke_off = sasl.createCommand("b21/smoke/off", "Disable wingtip smoke emitters")
 
+-- READ datarefs
 local DATAREF_TIME_S = globalPropertyf("sim/network/misc/network_time_sec")
 
 local command_time_s = 0.0
@@ -28,7 +31,7 @@ function clicked_smoke_off(phase)
 end
 
 function clicked_smoke_toggle(phase)
-  local time_now_s = get(DATAREF_TIME_S)
+  local time_now_s = get(DATAREF_TIME_S) -- use time delay to protect from switch bounce
   if time_now_s > command_time_s + 0.2 and phase == SASL_COMMAND_BEGIN
   then
     command_time_s = time_now_s
